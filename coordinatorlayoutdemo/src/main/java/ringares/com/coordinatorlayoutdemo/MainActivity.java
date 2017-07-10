aa8e5f07dccd0f5570621563da30480838ee4e6c
Sheng Lin <ringares@icloud.com>
2017-07-10 14:37:40 +0800

    commit

--- coordinatorlayoutdemo/src/main/java/ringares/com/coordinatorlayoutdemo/MainActivity.java
+++ coordinatorlayoutdemo/src/main/java/ringares/com/coordinatorlayoutdemo/MainActivity.java
@@ -3,6 +3,12 @@ package ringares.com.coordinatorlayoutdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 /**
@@ -20,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_2).setOnClickListener(this);
         findViewById(R.id.btn_3).setOnClickListener(this);
         findViewById(R.id.btn_4).setOnClickListener(this);
+        findViewById(R.id.btn_5).setOnClickListener(this);
     }
 
     @Override
@@ -41,6 +48,157 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_4:
                 TopBtmScrollActivity2.start(this);
                 break;
+
+            case R.id.btn_5:
+                System.out.println("isDeviceRooted()= " + isDeviceRooted());
+                System.out.println("isRooted()= " + isRooted());
+//                System.out.println("isRootGiven()= " + isRootGiven());
+
+                Toast.makeText(getApplicationContext(),
+                        "isDeviceRooted()= " + isDeviceRooted()
+                                + "\nisRooted()= " + isRooted()
+                                + "\nisRootGiven()= " + isRootGiven()
+                                + "\nisFinalRoot()= " + isFinalRoot()
+
+                        , Toast.LENGTH_LONG).show();
+                break;
         }
+    }
+
+    public static boolean isDeviceRooted() {
+        return checkRootMethod1() || checkRootMethod2() || checkRootMethod3();
+    }
+
+    private static boolean checkRootMethod1() {
+        String buildTags = android.os.Build.TAGS;
+        return buildTags != null && buildTags.contains("test-keys");
+    }
+
+    private static boolean checkRootMethod2() {
+        String[] paths = {"/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+                "/system/bin/failsafe/su", "/data/local/su", "/su/bin/su"};
+        for (String path : paths) {
+            if (new File(path).exists()) return true;
+        }
+        return false;
+    }
+
+    private static boolean checkRootMethod3() {
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(new String[]{"/system/xbin/which", "su"});
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            if (in.readLine() != null) return true;
+            return false;
+        } catch (Throwable t) {
+            return false;
+        } finally {
+            if (process != null) process.destroy();
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////
+    //
+    //
+    //
+    /////////////////////////////////////////////////////////////////
+    public static boolean findBinary(String binaryName) {
+        boolean found = false;
+        if (!found) {
+            String[] places = {"/sbin/", "/system/bin/", "/system/xbin/",
+                    "/data/local/xbin/", "/data/local/bin/",
+                    "/system/sd/xbin/", "/system/bin/failsafe/", "/data/local/", "/su/xbin"};
+            for (String where : places) {
+                if (new File(where + binaryName).exists()) {
+                    found = true;
+
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+
+    private static boolean isRooted() {
+        return findBinary("su");
+    }
+
+    /////////////////////////////////////////////////////////////////
+    //
+    //
+    //
+    /////////////////////////////////////////////////////////////////
+    public static boolean isRootAvailable() {
+        for (String pathDir : System.getenv("PATH").split(":")) {
+            if (new File(pathDir, "su").exists()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isRootGiven() {
+        if (isRootAvailable()) {
+            Process process = null;
+            try {
+                process = Runtime.getRuntime().exec(new String[]{"su", "-c", "id"});
+                BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String output = in.readLine();
+                if (output != null && output.toLowerCase().contains("uid=0"))
+                    return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (process != null)
+                    process.destroy();
+            }
+        }
+
+        return false;
+    }
+
+    /////////////////////////////////////////////////////////////////
+    //
+    //
+    //
+    /////////////////////////////////////////////////////////////////
+    private static boolean isFinalRoot() {
+
+        String[] places = {"/sbin/", "/system/bin/", "/system/xbin/",
+                "/data/local/xbin/", "/data/local/bin/",
+                "/system/sd/xbin/", "/system/bin/failsafe/", "/data/local/", "/su/xbin"};
+        for (String where : places) {
+            String pathname = where + "su";
+            if (new File(pathname).exists()) {
+                if (isExecutable(pathname)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isExecutable(String filePath) {
+        Process p = null;
+        try {
+            p = Runtime.getRuntime().exec("ls -l " + filePath);
+            // 获取返回内容
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    p.getInputStream()));
+            String str = in.readLine();
+            System.out.println("str: " + str);
+            if (str != null && str.length() >= 4) {
+                char flag = str.charAt(3);
+                if (flag == 's' || flag == 'x')
+                    return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (p != null) {
+                p.destroy();
+            }
+        }
+        return false;
     }
 }
--- coordinatorlayoutdemo/src/main/java/ringares/com/coordinatorlayoutdemo/TopNavBarHideActivity.java
+++ coordinatorlayoutdemo/src/main/java/ringares/com/coordinatorlayoutdemo/TopNavBarHideActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
 
 import java.util.ArrayList;
 
@@ -18,6 +24,7 @@ public class TopNavBarHideActivity extends AppCompatActivity {
 
     private RecyclerView rv;
     private DemoAdapter demoAdapter;
+    private View rootView;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, TopNavBarHideActivity.class);
@@ -31,17 +38,39 @@ public class TopNavBarHideActivity extends AppCompatActivity {
 
         ArrayList<DataBean> datas = new ArrayList<>();
         datas.add(new DataBean("Info", getString(R.string.text)));
-        datas.add(new DataBean("Info", getString(R.string.text)));
-        datas.add(new DataBean("Info", getString(R.string.text)));
-        datas.add(new DataBean("Info", getString(R.string.text)));
-        datas.add(new DataBean("Info", getString(R.string.text)));
-        datas.add(new DataBean("Info", getString(R.string.text)));
+//        datas.add(new DataBean("Info", getString(R.string.text)));
+//        datas.add(new DataBean("Info", getString(R.string.text)));
+//        datas.add(new DataBean("Info", getString(R.string.text)));
+//        datas.add(new DataBean("Info", getString(R.string.text)));
+//        datas.add(new DataBean("Info", getString(R.string.text)));
 
+        rootView = findViewById(R.id.main_content);
         rv = (RecyclerView) findViewById(R.id.rv);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         rv.setLayoutManager(layoutManager);
         demoAdapter = new DemoAdapter(R.layout.adapter_demo, datas);
         rv.setAdapter(demoAdapter);
+
+        rv.addOnItemTouchListener(new OnItemClickListener() {
+            @Override
+            public void onSimpleItemClick(BaseQuickAdapter adapter, View view, int position) {
+                Toast.makeText(getApplicationContext(), "pos: " + position, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+                if (e.getAction() == MotionEvent.ACTION_UP && rv.findChildViewUnder(e.getX(), e.getY()) == null) {
+                    System.out.println("onInterceptTouchEvent");
+
+                    return true;
+                } else {
+                    return super.onInterceptTouchEvent(rv, e);
+                }
+            }
+        });
+
+
     }
 }
